e9f531d32499a0c72c63732e55708bd92659463a
Dániel Móczár <moczardani@gmail.com>
2024-08-15 17:07:47 +0200

    Login page V1 done and some minor changes in registration page

--- services/frontend/flexnet-frontend/src/pages/Registration.jsx
+++ services/frontend/flexnet-frontend/src/pages/Registration.jsx
@@ -11,26 +11,26 @@ export default function Registration() {
             height: '100vh'
         }}>
             <Grid item xs={0} md={7} sx={{
-                py: 10,
                 px: 18,
-                display: { xs: 'none', md: 'flex'},
+                display: { xs: 'none', md: 'flex' },
                 flexDirection: 'column',
                 justifyContent: 'center',
                 alignItems: 'center',
+                gap: '10px',
                 background: 'linear-gradient(#5391f5, #DCEEFA)',
             }}>
-                <Typography variant='h2' sx={{color: 'white'}}>Join Us!</Typography>
-                <Typography variant='h5' sx={{color: 'white', textAlign: 'center'}}>Join the best fitness community in the world!</Typography>
+                <Typography variant='h2' sx={{ color: 'white' }}>Join Us!</Typography>
+                <Typography variant='h5' sx={{ color: 'white', textAlign: 'center' }}>Join FlexNet — your go-to fitness community where you can connect, share, and achieve your health and wellness goals together!</Typography>
                 <Box sx={{
-                    height: 400,
-                    width: 400,                                                    
-                }} component="img" src="../registration.svg"/>
+                    width: 400,
+                    mt: 4,
+                }} component="img" src="../registration.svg" />
             </Grid>
             <Grid item xs={12} md={5} sx={{
-                px: 6, 
+                px: 16,
                 display: 'flex',
                 justifyContent: 'center',
-                alignItems: 'center',    
+                alignItems: 'center',
             }}>
                 <RegistrationForm />
             </Grid>
