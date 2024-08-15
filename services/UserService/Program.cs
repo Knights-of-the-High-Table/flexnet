72cc3e507096a96641e6d66bf6eb6f4726f847c4
florianhennel <hennelflori@gmail.com>
2024-08-15 13:05:25 +0200

    change SwaggerDoc title and description

--- services/UserService/Program.cs
+++ services/UserService/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-     c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "UserService API", Description = "User related services", Version = "v1" });
 });
 
 var app = builder.Build();
@@ -15,7 +15,7 @@ if (app.Environment.IsDevelopment())
    app.UseSwagger();
    app.UseSwaggerUI(c =>
    {
-      c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+      c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserService API V1");
    });
 }
 
