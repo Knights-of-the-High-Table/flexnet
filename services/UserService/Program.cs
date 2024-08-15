0a697db121ea901626113545431ac7c8e0fae00f
florianhennel <hennelflori@gmail.com>
2024-08-15 18:51:01 +0200

    Create register endpoint using dapper to connect to database (may change in the future)

--- services/UserService/Program.cs
+++ services/UserService/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.OpenApi.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-     c.SwaggerDoc("v1", new OpenApiInfo { Title = "UserService API", Description = "User related services", Version = "v1" });
+   c.SwaggerDoc("v1", new OpenApiInfo { Title = "UserService API", Description = "User related services", Version = "v1" });
 });
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnecion");
 
 var app = builder.Build();
 
@@ -20,5 +24,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/register", async (User user) =>
+{
+   string sql = "INSERT INTO Users (Name, Email, Password, Height, Weight, Birthdate) VALUES (@Name, @Email, @Password, @Height, @Weight, @Birthdate);";
+   object[] parameters = [new { user.Name, user.Email, user.Password, user.Height, user.Weight, user.Birthdate }];
+    await using var connection = new SqlConnection(connectionString);
+    connection.Execute(sql, parameters);
+});
 
 app.Run();
