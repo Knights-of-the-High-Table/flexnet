b9d7e67597f28439789cb71d339fde2be80f6d15
Dániel Móczár <moczardani@gmail.com>
2024-08-21 11:42:39 +0200

    Basic post structure done

--- services/frontend/flexnet-frontend/src/pages/Home.jsx
+++ services/frontend/flexnet-frontend/src/pages/Home.jsx
@@ -1,7 +1,22 @@
+import { Grid } from '@mui/material';
 import Header from '../components/Header';
+import Post from '../components/Post';
 
 export default function Home() {
     return (
-        <Header />        
+        <>
+            <Header />
+            <Grid container sx={{mt: 2}}>
+                <Grid item md={3}>
+
+                </Grid>
+                <Grid item md={6}>
+                    <Post />
+                </Grid>
+                <Grid item md={3}>
+
+                </Grid>
+            </Grid>
+        </>
     )
 }
