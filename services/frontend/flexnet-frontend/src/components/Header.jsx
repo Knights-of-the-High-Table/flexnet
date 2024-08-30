463911a31e8783f2c5a53f5644699d697157c1a6
Dániel Móczár <moczardani@gmail.com>
2024-08-30 15:22:00 +0200

    Added notification popup

--- services/frontend/flexnet-frontend/src/components/Header.jsx
+++ services/frontend/flexnet-frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
-import { AppBar, Avatar, Box, Divider, Grid, IconButton, InputAdornment, Menu, MenuItem, Paper, TextField, Typography } from "@mui/material";
+import { AppBar, Avatar, Box, Divider, Grid, IconButton, InputAdornment, Menu, MenuItem, Paper, Popover, TextField, Tooltip, Typography } from "@mui/material";
 import HomeIcon from '@mui/icons-material/Home';
-import HandshakeIcon from '@mui/icons-material/Handshake';
+import PeopleIcon from '@mui/icons-material/People';
 import GroupsIcon from '@mui/icons-material/Groups';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import SearchIcon from '@mui/icons-material/Search';
@@ -13,6 +13,7 @@ import { useState } from "react";
 export default function Header({theme, page}) {
     const navigate = useNavigate();
     const [accountMenuOpen, setAccountMenuOpen] = useState(false);
+    const [notificationsOpen, setNotificationsOpen] = useState(false);
     const [anchorEl, setAnchorEl] = useState(null);
 
     const handleOpenAccountMenu = (event) => {
@@ -22,6 +23,15 @@ export default function Header({theme, page}) {
     const handleCloseAccountMenu = () => {
         setAnchorEl(null);
         setAccountMenuOpen(false);
+    }
+
+    const handleOpenNotifications = (event) => {
+        setAnchorEl(event.currentTarget);
+        setNotificationsOpen(true);
+    }
+    const handleCloseNotifications = () => {
+        setAnchorEl(null);
+        setNotificationsOpen(false);
     }
 
     return (
@@ -47,16 +57,22 @@ export default function Header({theme, page}) {
                         justifyContent: 'center',
                         alignItems: 'center',
                         gap: '30px'
-                    }}>     
-                        <IconButton onClick={() => navigate("/")}>
-                            <HomeIcon fontSize="large" sx={{color: page === "home" && theme.palette.primary.main}}/>
-                        </IconButton>
-                        <IconButton onClick={() => navigate("/friends")}>
-                            <HandshakeIcon fontSize="large" sx={{color: page === "friends" && theme.palette.primary.main}}/>
-                        </IconButton>
-                        <IconButton onClick={() => navigate("/groups")}>
-                            <GroupsIcon fontSize="large" sx={{color: page === "groups" && theme.palette.primary.main}}/>
-                        </IconButton>
+                    }}>   
+                        <Tooltip title="Home">
+                            <IconButton onClick={() => navigate("/")}>
+                                <HomeIcon fontSize="large" sx={{color: page === "home" && theme.palette.primary.main}}/>
+                            </IconButton>
+                        </Tooltip>  
+                        <Tooltip title="Friends">
+                            <IconButton onClick={() => navigate("/friends")}>
+                                <PeopleIcon fontSize="large" sx={{color: page === "friends" && theme.palette.primary.main}}/>
+                            </IconButton>
+                        </Tooltip>  
+                        <Tooltip title="Groups">
+                            <IconButton onClick={() => navigate("/groups")}>
+                                <GroupsIcon fontSize="large" sx={{color: page === "groups" && theme.palette.primary.main}}/>
+                            </IconButton>
+                        </Tooltip>  
                     </Grid>
                     <Grid item xs={4} sx={{
                         display: 'flex',
@@ -64,12 +80,16 @@ export default function Header({theme, page}) {
                         alignItems: 'center',
                         gap: '10px'
                     }}>     
-                        <IconButton>
-                            <NotificationsIcon fontSize="large"/>
-                        </IconButton>
-                        <IconButton onClick={handleOpenAccountMenu}>
-                            <Avatar>H</Avatar>
-                        </IconButton>
+                        <Tooltip title="Notifications">
+                            <IconButton onClick={handleOpenNotifications}>
+                                <NotificationsIcon fontSize="large"/>
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip title="My Account">
+                            <IconButton onClick={handleOpenAccountMenu}>
+                                <Avatar>H</Avatar>
+                            </IconButton>
+                        </Tooltip>
                     </Grid>
                 </Grid>
             </AppBar>
@@ -77,27 +97,53 @@ export default function Header({theme, page}) {
                 anchorEl={anchorEl}
                 open={accountMenuOpen}    
                 onClose={handleCloseAccountMenu}   
-            >
-                <MenuItem onClick={handleCloseAccountMenu}>
+            >                
+                <Typography variant="h5" sx={{fontWeight: 600, p: 2}}>My Account</Typography>
+                <MenuItem onClick={handleCloseAccountMenu} sx={{p: 2}}>
                     <Avatar>H</Avatar>
                     <Box sx={{display: 'flex', flexDirection: 'column'}}>
-                    <Typography variant="p" sx={{ml: 1, fontWeight: 600}}>Brian Brian</Typography>
-                    <Typography variant="subtitle2" sx={{ml: 1, color: theme.palette.grey[500]}}>View Profile</Typography>
+                    <Typography variant="p" sx={{ml: 1.5, fontWeight: 600}}>Brian Brian</Typography>
+                    <Typography variant="subtitle2" sx={{ml: 1.5, color: theme.palette.grey[500]}}>View Profile</Typography>
                     </Box>
                 </MenuItem>
                 <Divider />
-                <MenuItem onClick={handleCloseAccountMenu}>
+                <MenuItem onClick={handleCloseAccountMenu} sx={{width: '300px', p: 2}}>
                     <SettingsIcon />
-                    <Typography variant="p" sx={{ml: 1}}>Settings</Typography>
+                    <Typography variant="p" sx={{ml: 1.5}}>Settings</Typography>
                 </MenuItem>
-                <MenuItem onClick={handleCloseAccountMenu}>
+                <MenuItem onClick={handleCloseAccountMenu} sx={{p: 2}}>
                     <FeedbackIcon />
-                    <Typography variant="p" sx={{ml: 1}}>Send Feedback</Typography>
+                    <Typography variant="p" sx={{ml: 1.5}}>Send Feedback</Typography>
                 </MenuItem>
-                <MenuItem onClick={handleCloseAccountMenu}>
+                <MenuItem onClick={handleCloseAccountMenu} sx={{p: 2}}>
                     <LogoutIcon />
-                    <Typography variant="p" sx={{ml: 1}}>Log Out</Typography>
+                    <Typography variant="p" sx={{ml: 1.5}}>Log Out</Typography>
                 </MenuItem>
+            </Menu>
+
+            <Menu                
+                anchorEl={anchorEl}
+                open={notificationsOpen}
+                onClose={handleCloseNotifications}                      
+            >
+                <Typography variant="h5" sx={{fontWeight: 600, p: 2}}>Notifications</Typography>
+                <MenuItem sx={{whiteSpace: 'normal', wordBreak: 'break-word', width: '400px', p: 2}}>
+                    <Avatar>H</Avatar>
+                    <Typography variant="p" sx={{ml: 1.5}}>Brian Brian commented on your post.</Typography>
+                </MenuItem>
+                <MenuItem sx={{whiteSpace: 'normal', wordBreak: 'break-word', width: '400px', p: 2}}>
+                    <Avatar>H</Avatar>
+                    <Typography variant="p" sx={{ml: 1.5}}>Brian Brian commented on your post in the Fitness Tips.</Typography>
+                </MenuItem>
+                <MenuItem sx={{whiteSpace: 'normal', wordBreak: 'break-word', width: '400px', p: 2}}>
+                    <Avatar>H</Avatar>
+                    <Typography variant="p" sx={{ml: 1.5}}>Brian Brian commented on your post.</Typography>
+                </MenuItem>
+                <MenuItem sx={{whiteSpace: 'normal', wordBreak: 'break-word', width: '400px', p: 2}}>
+                    <Avatar>H</Avatar>
+                    <Typography variant="p" sx={{ml: 1.5}}>Brian Brian commented on your post.</Typography>
+                </MenuItem>
+
             </Menu>
         </>
     )
